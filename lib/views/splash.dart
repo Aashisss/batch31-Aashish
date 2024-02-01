32c4558cb8897fb4b63736dc55bf9323e1a21411
Aashish <95063987+Aashisss@users.noreply.github.com>
2024-02-01 12:06:04 +0545

    updated

--- lib/views/splash.dart
+++ lib/views/splash.dart
@@ -26,7 +26,7 @@ class _SplashState extends State<Splash> {
   route() {
     Navigator.of(context).pushReplacementNamed(AppRoute.loginRoute);
   }
-add
+
   @override
   Widget build(BuildContext context) {
     return Container(
